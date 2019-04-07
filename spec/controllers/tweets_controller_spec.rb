e736a1aa26ef3a69683e243abb751367e906ecb5
Cesar Cachay <cesarcachay1996@gmail.com>
2019-04-06 20:18:23 -0500

    Give format in tweets controller specs

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -31,18 +31,24 @@ describe TweetsController do
 
   describe 'GET show' do
     it 'returns http status ok' do
-      created_user = User.create(name: "Deyvi", description: 'Mi pelucaaaaa')
+      created_user = User.create(
+                      name: "Deyvi", 
+                      description: 'Mi pelucaaaaa'
+                      )
       tweets = Tweet.create(
-              content: 'Estoy testeando show!',
-              user: created_user
-              )
+                content: 'Estoy testeando show!',
+                user: created_user
+                )
             get :show, params: { id: tweets.id }
       expect(response).to have_http_status(:ok)
     end
 
 
     it 'render the correct tweet' do
-      created_user = User.create(name: "Deyvi", description: 'Mi pelucaaaaa')
+      created_user = User.create(
+                      name: "Deyvi", 
+                      description: 'Mi pelucaaaaa'
+                      )
       tweets = Tweet.create(
                 content: "Estoy mostrando el correcto tweet", 
                 user: created_user
@@ -61,7 +67,10 @@ describe TweetsController do
 
   describe "POST create" do
     it "returns http status created" do
-      created_user = User.create(name: "Brayan", description: 'Mis tests!')
+      created_user = User.create(
+        name: "Brayan", 
+        description: 'Mis tests!'
+        )
       post :create, params: { content: "Este tweet ha sido creado!", user: created_user}
       expect(response.status).to eq(201)
       expect(response).to have_http_status(:created)
@@ -78,15 +87,27 @@ describe TweetsController do
 
   describe "PATCH update" do
     it "returns http status ok" do
-      created_user = User.create(name: "Carlos", description: 'Software developer!')
-      tweets = Tweet.create(content: "Estoy testeando update!", user_id: created_user.id)
+      created_user = User.create(
+                      name: "Carlos", 
+                      description: 'Software developer!'
+                      )
+      tweets = Tweet.create(
+                content: "Estoy testeando update!", 
+                user_id: created_user.id
+                )
       patch :update, params: { id: tweets.id}
       expect(response).to have_http_status(:ok)
     end
 
     it "returns the updated product" do
-      created_user = User.create(name: "Carlos", description: 'Software developer!')
-      tweets = Tweet.create(content: "Estoy testeando update!", user_id: created_user.id)
+      created_user = User.create(
+                      name: "Carlos", 
+                      description: 'Software developer!'
+                      )
+      tweets = Tweet.create(
+                content: "Estoy testeando update!", 
+                user_id: created_user.id
+                )
       patch :update, params: {  id: tweets.id, content: "Updated!" }
       expected_tweet = JSON.parse(response.body)
       expect(expected_tweet["content"]).to eq("Updated!")
@@ -96,22 +117,40 @@ describe TweetsController do
 
   describe "DELETE destroy" do
     it "returns http status no content" do
-      created_user = User.create(name: "Cesar", description: 'Ruby developer!')
-      tweets = Tweet.create(content: 'Testing DELETE method!', user_id: created_user.id)
+      created_user = User.create(
+                      name: "Cesar", 
+                      description: 'Ruby developer!'
+                      )
+      tweets = Tweet.create(
+                content: 'Testing DELETE method!', 
+                user_id: created_user.id
+                )
       delete :destroy, params: { id: tweets.id }
       expect(response).to have_http_status(:no_content)
     end
 
     it "returns empty body" do
-      created_user = User.create(name: "Angie", description: 'Developing my twitter API!')
-      tweets = Tweet.create(content: 'Testeando el método destroy', user_id: created_user.id)
+      created_user = User.create(
+                      name: "Angie", 
+                      description: 'Developing my twitter API!'
+                      )
+      tweets = Tweet.create(
+                content: 'Testeando el método destroy', 
+                user_id: created_user.id
+                )
       delete :destroy, params: { id: tweets.id }
       expect(response.body).to eq(" ")
     end
 
     it "actually delete the Tweet" do
-      created_user = User.create(name: "Sergio", description: 'Full Stack Software Developer!')
-      created_tweet = Tweet.create(content: "Esto será destruido", user_id: created_user.id)
+      created_user = User.create(
+                      name: "Sergio", 
+                      description: 'Full Stack Software Developer!'
+                      )
+      created_tweet = Tweet.create(
+                      content: "Esto será destruido", 
+                      user_id: created_user.id
+                      )
       tweets = Tweet.where(user_id: created_user.id)
       delete :destroy, params: { id: created_tweet.id }
       expect(tweets.size).to eq(0)
